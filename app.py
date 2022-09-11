3e84fb84a2f3b381f3d6cb1b4d056f9477947575
Vicente <vicentesaavedra45@gmail.com>
2022-09-11 17:38:48 -0500

    servidor

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 app.config[
     "MONGO_URI"
-] = "mongodb+srv://OtterFox:sqbZeoBKy8Qp6fd@cluster0.uwltrza.mongodb.net/mineria"
+] = "mongodb+srv://OtterFox:sqbZeoBKy8Qp6fd9@cluster0.uwltrza.mongodb.net/mineria"
 
 mongo = PyMongo(app)
 
